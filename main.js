1c8902bc04518ab3686ad1d410f42584dd3fa824
kesharaJayasinghe <kesharalahiru1@gmail.com>
2020-04-26 11:09:54 +0530

    Replaced form with contact email

--- main.js
+++ main.js
@@ -35,6 +35,21 @@ $(document).ready(function () {
     duration: 1800,
     // once: true   // stop animation once loaded
   });
+
+  let modalBtn = document.getElementById("button");
+  let modal = document.querySelector(".modal");
+  let closeBtn = document.querySelector(".close-btn");
+  modalBtn.onclick = function () {
+    modal.style.display = "flex";
+  };
+  closeBtn.onclick = function () {
+    modal.style.display = "none";
+  };
+  window.onclick = function (e) {
+    if (e.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 });
 
 /* ******Typing Carousel Start****** */
@@ -100,13 +115,13 @@ window.onload = function () {
 
 /* ******Particle animate start****** */
 if (document.body.animate) {
-  document.querySelector('#button').addEventListener('click', pop);
+  document.querySelector("#button").addEventListener("click", pop);
 }
 
-function pop (e) {
+function pop(e) {
   // Quick check if user clicked the button using a keyboard
   if (e.clientX === 0 && e.clientY === 0) {
-    const bbox = document.querySelector('#button').getBoundingClientRect();
+    const bbox = document.querySelector("#button").getBoundingClientRect();
     const x = bbox.left + bbox.width / 2;
     const y = bbox.top + bbox.height / 2;
     for (let i = 0; i < 30; i++) {
@@ -123,42 +138,45 @@ function pop (e) {
   }
 }
 
-function createParticle (x, y) {
-  const particle = document.createElement('particle');
+function createParticle(x, y) {
+  const particle = document.createElement("particle");
   document.body.appendChild(particle);
-  
+
   // Calculate a random size from 5px to 25px
   const size = Math.floor(Math.random() * 12 + 5);
   particle.style.width = `${size}px`;
   particle.style.height = `${size}px`;
   // Generate a random color in a blue/purple palette
   particle.style.background = `hsl(${Math.random() * 90 + 90}, 50%, 40%)`;
-  
+
   // Generate a random x & y destination within a distance of 75px from the mouse
   const destinationX = x + (Math.random() - 0.5) * 2 * 75;
   const destinationY = y + (Math.random() - 0.5) * 2 * 75;
 
   // Store the animation in a variable as we will need it later
-  const animation = particle.animate([
+  const animation = particle.animate(
+    [
+      {
+        // Set the origin position of the particle
+        // We offset the particle with half its size to center it around the mouse
+        transform: `translate(-50%, -50%) translate(${x}px, ${y}px)`,
+        opacity: 1,
+      },
+      {
+        // We define the final coordinates as the second keyframe
+        transform: `translate(${destinationX}px, ${destinationY}px)`,
+        opacity: 0,
+      },
+    ],
     {
-      // Set the origin position of the particle
-      // We offset the particle with half its size to center it around the mouse
-      transform: `translate(-50%, -50%) translate(${x}px, ${y}px)`,
-      opacity: 1
-    },
-    {
-      // We define the final coordinates as the second keyframe
-      transform: `translate(${destinationX}px, ${destinationY}px)`,
-      opacity: 0
+      // Set a random duration from 500 to 1500ms
+      duration: Math.random() * 1000 + 500,
+      easing: "cubic-bezier(0, .9, .57, 1)",
+      // Delay every particle with a random value of 200ms
+      delay: Math.random() * 200,
     }
-  ], {
-    // Set a random duration from 500 to 1500ms
-    duration: Math.random() * 1000 + 500,
-    easing: 'cubic-bezier(0, .9, .57, 1)',
-    // Delay every particle with a random value of 200ms
-    delay: Math.random() * 200
-  });
-  
+  );
+
   // When the animation is complete, remove the element from the DOM
   animation.onfinish = () => {
     particle.remove();
