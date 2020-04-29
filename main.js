097304c6a322c02c2ff1ff01e5d36a6635392e37
kesharaJayasinghe <kesharalahiru1@gmail.com>
2020-04-29 10:35:28 +0530

    Content update

--- main.js
+++ main.js
@@ -200,11 +200,10 @@ function copyToClipboard() {
   document.execCommand("copy");
   window.getSelection().removeAllRanges();
   // alert("Copied to Clipboard!");
-  var x = document.getElementById("snackbar");
 
+  var x = document.getElementById("snackbar");
   // Add the "show" class to DIV
   x.className = "show";
-
   // After 3 seconds, remove the show class from DIV
   setTimeout(function () {
     x.className = x.className.replace("show", "");
